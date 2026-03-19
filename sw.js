32fd3908def3fcf7299517230c8dadd21e5af8b4
aaron <33557557+aaronhalstonlee@users.noreply.github.com>
2026-03-19 14:59:59 -0700

    Update service worker cache version to v11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gm-trip-planner-v8';
+const CACHE = 'gm-trip-planner-v11';
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./'])));
@@ -9,6 +9,7 @@ self.addEventListener('activate', e => {
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     )
   );
+  self.clients.claim();
 });
 self.addEventListener('fetch', e => e.respondWith(
   caches.match(e.request).then(r => r || fetch(e.request))
