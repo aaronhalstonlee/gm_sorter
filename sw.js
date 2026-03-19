7924404dfdf51465c7aa71cce4d85578ead3c975
aaron <33557557+aaronhalstonlee@users.noreply.github.com>
2026-03-19 15:13:04 -0700

    Update service worker event listeners
    Refactor service worker to unregister old cache and navigate clients.

--- sw.js
+++ sw.js
@@ -1,16 +1,5 @@
-const CACHE = 'gm-trip-planner-v11';
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./'])));
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => {
+  self.registration.unregister();
+  self.clients.matchAll().then(clients => clients.forEach(c => c.navigate(c.url)));
 });
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => e.respondWith(
-  caches.match(e.request).then(r => r || fetch(e.request))
-));
